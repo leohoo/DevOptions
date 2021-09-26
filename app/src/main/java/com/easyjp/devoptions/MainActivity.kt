bec0a6df00c498259acf23389a00dbead5ba63a9
Wei Liu <leohoo@hotmail.com>
2021-09-26 23:08:24 +0900

    shortcut menu impl; update build settings

--- app/src/main/java/com/easyjp/devoptions/MainActivity.kt
+++ app/src/main/java/com/easyjp/devoptions/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.easyjp.devoptions
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
         val dev = Intent("com.android.settings.APPLICATION_DEVELOPMENT_SETTINGS")
         startActivity(dev)
